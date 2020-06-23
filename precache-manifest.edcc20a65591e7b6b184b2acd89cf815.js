07b948d04593c9d671689204dc0db10b73985229
Ethan Irvine <edog.irvine@gmail.com>
2020-06-23 11:56:13 -0700

    Updates

---
+++ precache-manifest.edcc20a65591e7b6b184b2acd89cf815.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee27c8d20d261a4d6a7ecd73fd69356d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27507fba83408863936",
+    "url": "/static/css/main.a5e72565.chunk.css"
+  },
+  {
+    "revision": "268b81c78e4de50ff89b",
+    "url": "/static/js/2.1122ceb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1122ceb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27507fba83408863936",
+    "url": "/static/js/main.167530dc.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "3097851268012cd982577d5143e5d9fc",
+    "url": "/static/media/crit-fail.30978512.png"
+  },
+  {
+    "revision": "667ade0fbc22d7c199078d939334ee01",
+    "url": "/static/media/notes.667ade0f.png"
+  },
+  {
+    "revision": "2585c212eed6f5b6d6f220e0950a7cd7",
+    "url": "/static/media/password.2585c212.png"
+  },
+  {
+    "revision": "e67494f3c3e18830c7fda0fefb1a7c50",
+    "url": "/static/media/planner.e67494f3.png"
+  },
+  {
+    "revision": "37d35bb75b8f6fd3bdf537679125f222",
+    "url": "/static/media/resume.37d35bb7.png"
+  },
+  {
+    "revision": "d60e710185774c085887f3f42a619dcc",
+    "url": "/static/media/theStreet.d60e7101.png"
+  },
+  {
+    "revision": "dadd1dc583e274664f8d3d39f526419a",
+    "url": "/static/media/weather.dadd1dc5.png"
+  }
+]);
